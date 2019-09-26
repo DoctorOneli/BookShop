1a1197936a78387c821a5de1be75ed9eef802065
DoctorOneli <1006629349@qq.com>
2019-09-26 23:08:56 +0800

    添加Thymeleaf

--- boot/src/main/java/org/freedom/boot/TestController.java
+++ boot/src/main/java/org/freedom/boot/TestController.java
@@ -5,28 +5,35 @@ import java.util.Date;
 import org.freedom.boot.bean.Msg;
 import org.freedom.boot.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
 
-	@Autowired
-	BookService bookService;
-	
-	@GetMapping("/hello")
-	public String hello()
+	@RequestMapping("/test")
+	public ModelAndView test(Model model)
 	{
-		return "hello";
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
 	}
-	
-	@GetMapping("/hi")
-	public String hi()
-	{
-		return "hi,hi";
-	}
-	
 	 
+	@RequestMapping("/register")
+	public ModelAndView Register(@ModelAttribute("form") User user) {  // user:视图层传给控制层的表单对象；model：控制层返回给视图层的对象
+        // 在 model 中添加一个名为 "user" 的 user 对象
+		System.out.println(user.getUsername());
+		
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("success");
+		return modelAndView;
+}
+
 	
 	 
 }
